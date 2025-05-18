b8a97c6aecccf182f55b6e41f4a6005b05db9096
Nelshon99 <nelson.hernandezbadel@gmail.com>
2025-05-18 16:23:03 -0500

    MouseTraker funcional

--- proyects/03-MouseTraker/src/App.jsx
+++ proyects/03-MouseTraker/src/App.jsx
@@ -1,12 +1,45 @@
-//import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
 
+  const [enabled, setEnable] = useState(false)
+  const [posicion, setPosicion] = useState({ x: 0, y: 0 })
+  useEffect(() => {
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosicion({ x: clientX, y: clientY })
+    }
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
 
   return (
-    <h1>HolaxD</h1>
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${posicion.x}px, ${posicion.y}px)`
+      }}
+      />
+      <button onClick={() => setEnable(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} Seguir mouse</button>
+    </>
   )
+
 }
 
 export default App
