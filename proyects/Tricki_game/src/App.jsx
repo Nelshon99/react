c51bcaa0bf6f7a584d3e07869b38dece042b537d
Nelshon99 <nelson.hernandezbadel@gmail.com>
2025-05-18 00:52:02 -0500

    Tricky Funcional

--- proyects/Tricki_game/src/App.jsx
+++ proyects/Tricki_game/src/App.jsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Children, useState } from 'react'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const TURNS = {
+    X: 'x',
+    O: 'o'
 }
 
-export default App
+
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+    const clasName = `square ${isSelected ? 'is-selected' : ''}`
+    const handleClick = () => {
+        updateBoard(index)
+    }
+    return (
+        <div onClick={handleClick} className={clasName}>
+            {children}
+        </div>
+    )
+}
+
+const WINNERS_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2]
+]
+
+export function App() {
+    const [board, setBoard] = useState(Array(9).fill(null))
+    const [turn, setTurn] = useState(TURNS.X)
+    //Null (No ganador) / false empate
+    const [winner, setWinner] = useState(null)
+
+    const checkWinner = (boardToCheck) => {
+        for (const combo of WINNERS_COMBOS) {
+            const [a, b, c] = combo
+            if (
+                boardToCheck[a] &&
+                boardToCheck[a] === boardToCheck[b] &&
+                boardToCheck[a] === boardToCheck[c]
+            ) {
+                return boardToCheck[a]
+            }
+        }
+        //No winner
+        return (null)
+
+    }
+
+    const resetGame = () => {
+        setBoard(Array(9).fill(null))
+        setTurn(TURNS.X)
+        setWinner(null)
+    }
+
+    //Check fin del juego
+    const checkEndGame = (newBoard) => {
+        return newBoard.every((square) => square !== null)
+    }
+
+    const updateBoard = (index) => {
+        //No sobreescribir la misma casilla
+        if (board[index]) return
+        //Actualiza el tablero
+        const newBoard = [...board]
+        newBoard[index] = turn
+        setBoard(newBoard)
+
+        const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+        setTurn(newTurn)
+        //Check Winner
+        const newWinner = checkWinner(newBoard)
+        if (newWinner) {
+            setWinner(newWinner)
+        } else if (checkEndGame(newBoard)) {
+            setWinner(false) //Empate
+        }
+    }
+    return (
+        <main className="board">
+            <h1>Tricky</h1>
+            <button onClick={resetGame}>Reset Juego</button>
+            <section className="game">
+                {
+                    board.map((_, index) => {
+                        return (
+                            <Square
+                                key={index}
+                                index={index}
+                                updateBoard={updateBoard}
+                            >
+                                {board[index]}
+                            </Square>
+                        )
+                    })
+                }
+            </section>
+
+            <section className='turn'>
+                <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+                <Square isSelected={turn === TURNS.O} > {TURNS.O}</Square>
+            </section>
+
+            {
+                winner !== null && (
+                    <section className='winner'>
+                        <div className='text'>
+                            <h2>
+                                {
+                                    winner === false
+                                        ? 'Empate'
+                                        : 'Gano:'
+                                }
+                            </h2>
+                            <header className='win'>
+                                {winner && <Square>{winner} </Square>}
+                            </header>
+                            <footer>
+                                <button onClick={resetGame}>Empezar de nuevo</button>
+                            </footer>
+                        </div>
+                    </section>
+                )
+            }
+
+        </main >
+    )
+}
